c67fc260ceb412fe9ac40adf7b0cb4212bfcda35
LLOEVSES <lzy.lizhengyu@gmail.com>
2020-06-29 16:14:32 +0800

    first commit

---
+++ precache-manifest.d0b9e573262b3b1ba361397c7f3a541c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9095f7e3405158a9a2dfed62c9c6fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aa4e1277183bf29ded2",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "090443af831c83f44ee1",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6aa4e1277183bf29ded2",
+    "url": "/static/js/2.f606c184.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f606c184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "090443af831c83f44ee1",
+    "url": "/static/js/main.6f9c365a.chunk.js"
+  },
+  {
+    "revision": "c59bcba4a93c54ecf3ea",
+    "url": "/static/js/runtime-main.4c3a98ff.js"
+  }
+]);
